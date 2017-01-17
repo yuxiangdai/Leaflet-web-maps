85941f3bb83ac29804e51af6241ebaedee58d621
Yuxiang Dai <yuxiang.dai43@gmail.com>
2017-01-16 23:26:36 -0500

    elevation color function

--- script.py
+++ script.py
@@ -6,9 +6,18 @@ df = pandas.read_csv("Volcanoes_USA.txt")
 
 map = folium.Map(location=[45.372,-121.697], zoom_start=4, tiles='Stamen Terrain')
 
+def color(elev):
+    if elev in range(0,1000):
+        col='green'
+    elif elev in range(1000,3000):
+        col='orange'
+    else:
+        col='red'
+    return col
+
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
     
-    map.simple_marker(location=[lat,lon],popup=name, marker_color='green' if elev in range(0,1000) else 'orange' if elev in range (1000,3000) else 'red')
+    map.simple_marker(location=[lat,lon],popup=name, marker_color=color(elev))
 
 
 map.create_map(path='test.html')
