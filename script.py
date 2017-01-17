8206533065f1ba4b6b7e8634eba9c239a2f434c9
Yuxiang Dai <yuxiang.dai43@gmail.com>
2017-01-16 23:13:53 -0500

    dynamic coloring and read from CSV data

--- script.py
+++ script.py
@@ -1,6 +1,14 @@
 import folium
-map = folium.Map(location=[45.372,-121.697], zoom_start=12, tiles='Stamen Terrain')
-map.simple_marker(location=[45.372,-121.697],popup='Mt. Hood Meadows', marker_color='red')
-map.simple_marker(location=[45.372,-176.697],popup='Timberland Place', marker_color='green')
+import pandas
+
+
+df = pandas.read_csv("Volcanoes_USA.txt")
+
+map = folium.Map(location=[45.372,-121.697], zoom_start=4, tiles='Stamen Terrain')
+
+for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
+    
+    map.simple_marker(location=[lat,lon],popup=name, marker_color='green' if elev in range(0,1000) else 'orange' if elev in range (1000,3000) else 'red')
+
 
 map.create_map(path='test.html')
