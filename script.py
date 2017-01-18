748460a70bdd18b79cc87a0ff69f6d3084d0ed76
Yuxiang Dai <yuxiang.dai43@gmail.com>
2017-01-17 22:27:00 -0500

    update for new Folium version

--- script.py
+++ script.py
@@ -1,23 +1,22 @@
 import folium
 import pandas
 
-
 df = pandas.read_csv("Volcanoes_USA.txt")
 
-map = folium.Map(location=[45.372,-121.697], zoom_start=4, tiles='Stamen Terrain')
+map = folium.Map(location=[df['LAT'].mean(), df['LON'].mean()], zoom_start=4, tiles='Stamen Terrain')
 
 def color(elev):
-    if elev in range(0,1000):
+    minimum = int(min(df['ELEV']))
+    step = int((max(df['ELEV'])-min(df['ELEV']))/3)
+    if elev in range(minimum, minimum + step):
         col='green'
-    elif elev in range(1000,3000):
+    elif elev in range(minimum + step, minimum + step*2):
         col='orange'
     else:
         col='red'
     return col
 
 for lat, lon, name, elev in zip(df['LAT'], df['LON'], df['NAME'], df['ELEV']):
-    
-    map.simple_marker(location=[lat,lon],popup=name, marker_color=color(elev))
+    map.add_child(folium.Marker(location=[lat,lon],popup=name, icon=folium.Icon(color(elev))))
 
-
-map.create_map(path='test.html')
+map.save(outfile='test.html')
